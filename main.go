d5c55fa88d02e93b1f039cfc85a02fc294bad53e
thangtmc73 <thangtm.dev@gmail.com>
2022-02-20 11:57:09 +0700

    feat: connect to the dabase with the connection string from env

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"os"
+	"time"
+)
 
 func main() {
-  fmt.Println("Hello World")
+	dsn := os.Getenv("DBConnectionStr")
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err)
+	}
 }
